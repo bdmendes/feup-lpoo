7209c8fc0d1bd68b4c7c967c053925ba8561726e
Bruno Mendes <brod56@sapo.pt>
2021-02-17 10:21:53 +0000

    Add friendlier message

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,5 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Thanks for using Hello world app");
     }
 }
