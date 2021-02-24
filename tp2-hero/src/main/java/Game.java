22729fb6f9c2509ed03bc8a3cdd57b6798b33a9e
Bruno Mendes <brod56@sapo.pt>
2021-02-24 10:42:42 +0000

    Move dot with the keyboard

--- tp2-hero/src/main/java/Game.java
+++ tp2-hero/src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
@@ -9,9 +11,13 @@ import java.io.IOException;
 
 public class Game {
     private final Screen screen;
+    private int x = 10;
+    private int y = 10;
+    private int columns = 40;
+    private int rows = 20;
 
     Game() throws IOException {
-        TerminalSize terminalSize = new TerminalSize(40, 20);
+        TerminalSize terminalSize = new TerminalSize(this.columns, this.rows);
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory()
                 .setInitialTerminalSize(terminalSize);
         Terminal terminal = terminalFactory.createTerminal();
@@ -19,7 +25,12 @@ public class Game {
     }
 
     public void run() throws IOException {
-        this.draw();
+        for(;;){
+            this.draw();
+            KeyStroke key = this.screen.readInput();
+            if (key.getKeyType() == KeyType.EOF) break;
+            this.processKey(key);
+        }
     }
 
     private void draw() throws IOException {
@@ -28,7 +39,32 @@ public class Game {
         this.screen.doResizeIfNecessary();     // resize screen if necessary
 
         this.screen.clear();
-        this.screen.setCharacter(10, 10, TextCharacter.fromCharacter('A')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         this.screen.refresh();
     }
+
+    private void processKey(KeyStroke key) throws IOException {
+        System.out.println(key);
+        switch (key.getKeyType()){
+            case ArrowUp:
+                if (y > 0) y--;
+                break;
+            case ArrowDown:
+                if (y < rows - 1) y++;
+                break;
+            case ArrowLeft:
+                if (x > 0) x--;
+                break;
+            case ArrowRight:
+                if (x < columns - 1) x++;
+                break;
+            case Character:
+                char c = key.getCharacter();
+                if (c == 'q') this.screen.close(); // sends eof signal
+                break;
+            default:
+                break;
+        }
+    }
+
 }
