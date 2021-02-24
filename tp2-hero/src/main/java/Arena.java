f33f2d756b9a12cec63a9b15a046e032c910c22a
Bruno Mendes <brod56@sapo.pt>
2021-02-24 12:02:39 +0000

    Add walls

--- tp2-hero/src/main/java/Arena.java
+++ tp2-hero/src/main/java/Arena.java
@@ -4,15 +4,20 @@ import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arena {
     int width;
     int height;
     private final Hero hero;
+    private List<Wall> walls;
 
     Arena(int width, int height){
         this.height = height;
         this.width = width;
         this.hero = new Hero(10,10);
+        this.walls = this.createWalls();
     }
 
     public Hero getHero() {
@@ -23,6 +28,9 @@ public class Arena {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
         this.hero.draw(graphics);
+        for (Wall wall : walls){
+            wall.draw(graphics);
+        }
     }
 
     public void processKey(KeyStroke key) {
@@ -55,4 +63,18 @@ public class Arena {
         return position.getX() >= 0 && position.getX() < width
                 && position.getY() >= 0 && position.getY() < height;
     }
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+        return walls;
+    }
+
 }
---
+++ tp2-hero/src/main/java/Wall.java
@@ -0,0 +1,19 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+import java.util.List;
+
+public class Wall {
+    private final Position position;
+
+    public Wall(int x, int y){
+        this.position = new Position(x,y);
+    }
+    public void draw(TextGraphics graphics){
+        graphics.setForegroundColor(TextColor.Factory.fromString("#00FF00"));
+        graphics.enableModifiers(SGR.ITALIC);
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "X");
+    }
+}
