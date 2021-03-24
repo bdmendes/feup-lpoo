e4edf4a881d1e2980e3d03b25ab8c6cb4f499ecd
Bruno Mendes <brod56@sapo.pt>
2021-03-24 11:11:34 +0000

    Add composite pattern

---
+++ tp5-design-patterns/src/main/java/String/StringTransformerGroup.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class StringTransformerGroup implements StringTransformer {
+
+    List<StringTransformer> transformers;
+
+    public StringTransformerGroup(List<StringTransformer> transformers){
+        this.transformers = transformers;
+    }
+
+    @Override
+    public void execute(StringDrink drink) {
+        for (StringTransformer t : transformers) {
+            t.execute(drink);
+        }
+    }
+
+    @Override
+    public void undo(StringDrink drink) {
+        for (int i = transformers.size() - 1; i >= 0; i--) {
+            transformers.get(i).execute(drink);
+        }
+    }
+}
