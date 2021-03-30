aa39cae208f45c9617d03ab6aa272be7bdf21df4
Bruno Mendes <brod56@sapo.pt>
2021-03-30 18:21:23 +0100

    Add 2021 test example

---
+++ practical_tests/2021-test/src/Thing.java
@@ -0,0 +1,15 @@
+public class Thing {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private int id;
+
+    public Thing(int id) {
+        this.id = id;
+    }
+}
